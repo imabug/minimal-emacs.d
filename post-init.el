fe72a88f067c08013aeb043c6c2d9b71908edc53
Eugene Mah <eugenemah@gmail.com>
2026-08-30 09:59:49 -0400

    Update php-ts-mode version

--- post-init.el
+++ post-init.el
@@ -634,7 +634,7 @@
         (julia "https://github.com/tree-sitter/tree-sitter-julia")
         (make "https://github.com/alemuller/tree-sitter-make")
         (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-        (php "https://github.com/tree-sitter/tree-sitter-php" "v0.23.12" "php/src")
+        (php "https://github.com/tree-sitter/tree-sitter-php" "v0.24.2" "php/src")
         (phpdoc "https://github.com/claytonrcarter/tree-sitter-phpdoc")
         (python "https://github.com/tree-sitter/tree-sitter-python")
         (rust "https://github.com/tree-sitter/tree-sitter-rust")
